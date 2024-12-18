4461595145846c518aca680afd199e96ebd36878
Aarosh Das <aaroshdas@gmail.com>
2024-12-17 19:37:02 -0500

    gen

--- shakespeare-generate.py
+++ shakespeare-generate.py
@@ -62,41 +62,40 @@ def generate(wL,wS, b, activationFunc, letters):
     dots[(len(wL)-1, len(inp))] =wL[len(wL)-1]@As[(len(wL)-2, len(inp))]
     As[(len(wL)-1, len(inp))] = softmax(dots[(len(wL)-1, len(inp))])
 
-    print(As[(len(wL)-1, len(inp))])
-    
     weights = []
     options = []
     for i in range(39):
         weights.append(As[(len(wL)-1, len(inp))][i,0])
         options.append(oneHotVectorsKeys[i])
     res = choices(options, weights=weights, k=1)
-    print(res[0])
+    # print(res[0])
 
-    maxI = 0
-    for i in range(39):
-        if(As[(len(wL)-1, len(inp))][i,0] > As[(len(wL)-1, len(inp))][maxI, 0]):
-            maxI = i
-    res= [oneHotVectorsKeys[maxI]]
+    # maxI = 0
+    # for i in range(39):
+    #     if(As[(len(wL)-1, len(inp))][i,0] > As[(len(wL)-1, len(inp))][maxI, 0]):
+    #         maxI = i
+    # res= [oneHotVectorsKeys[maxI]]
+    # print(As[(len(wL)-1, len(inp))])
 
     return res[0]
 
-temprature=1
-
-with open("shakespeare-tests.pkl", "rb") as f:
-    with open("w_b_current.pkl", "rb") as f:
-        w1L, w1S, b1, currIdx = pickle.load(f)
-        print(currIdx)
-    print(generate(w1L, w1S, b1, np.tanh, "First citize".lower()))
-print(oneHotVectorsKeys)
+temprature=0.7
 
 # with open("shakespeare-tests.pkl", "rb") as f:
 #     with open("w_b_current.pkl", "rb") as f:
 #         w1L, w1S, b1, currIdx = pickle.load(f)
 #         print(currIdx)
-#     generation = "".lower()
-#     while len(generation) < 150:
-#         generation+= generate(w1L, w1S, b1, np.tanh, generation[-100:-1])
-#     print("final text:")
-#     print(generation)
+#     print(generate(w1L, w1S, b1, np.tanh, "First citize".lower()))
+# print(oneHotVectorsKeys)
+
+with open("shakespeare-tests.pkl", "rb") as f:
+    with open("w_b_1200000.pkl", "rb") as f:
+        w1L, w1S, b1, currIdx = pickle.load(f)
+        print(currIdx)
+    generation = "the ".lower()
+    while len(generation) < 250:
+        generation+= generate(w1L, w1S, b1, np.tanh, generation[-100:])
+    print("final text:")
+    print(generation)
 
 
